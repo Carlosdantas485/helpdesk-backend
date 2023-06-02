71f4d28b7cf6d4ed8f9f498c99b7a11063950630
CarlosDantas <carlosdantas485@gmail.com>
2023-06-02 12:36:16 -0300

    Created test profile

--- src/main/java/com/dantas/helpdesk/config/TestConfig.java
+++ src/main/java/com/dantas/helpdesk/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.dantas.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,13 +9,19 @@ import org.springframework.context.annotation.Profile;
 import com.dantas.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void DBInstance() {
-	this.dbService.DBInstance();
+	public boolean DBInstance() {
+		if(value.equals("create")) {
+			this.dbService.DBInstance();
+		}
+		return false;
 	}
 }
