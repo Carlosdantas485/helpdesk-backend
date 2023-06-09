4082fe32bb5ded7a782a8000b5ce9a23f4b3b509
CarlosDantas <carlosdantas485@gmail.com>
2023-06-09 11:55:25 -0300

    exceptions handling to CPF and Email

--- src/main/java/com/dantas/helpdesk/domain/repositories/PeopleRepository.java
+++ src/main/java/com/dantas/helpdesk/domain/repositories/PeopleRepository.java
@@ -1,10 +1,16 @@
 package com.dantas.helpdesk.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dantas.helpdesk.domain.People;
 
 
-public interface PeopleRepository extends JpaRepository<People, Integer> {
 
+public interface PeopleRepository extends JpaRepository<People, Integer> {
+	Optional<People> findByCpf(String cpf);
+	Optional<People> findByEmail(String email);
+	
+	
 }
--- src/main/java/com/dantas/helpdesk/services/TecnicService.java
+++ src/main/java/com/dantas/helpdesk/services/TecnicService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dantas.helpdesk.domain.People;
 import com.dantas.helpdesk.domain.Tecnic;
 import com.dantas.helpdesk.domain.dtos.TecnicDTO;
+import com.dantas.helpdesk.domain.repositories.PeopleRepository;
 import com.dantas.helpdesk.domain.repositories.TecnicRepository;
+import com.dantas.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.dantas.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicService {
 	
 	@Autowired
 	private TecnicRepository repository;
+	
+	@Autowired
+	private PeopleRepository peopleRepository;
 	
 	public Tecnic findById(Integer id) {
 		Optional<Tecnic> obj = repository.findById(id);
@@ -29,9 +35,23 @@ public class TecnicService {
 	public Tecnic create(TecnicDTO objDTO) {
 		
 		objDTO.setId(null);
+		CPFvalidateAndEmail(objDTO);
 		Tecnic newObj = new Tecnic(objDTO);
-		
 		return repository.save(newObj);
+	}
+
+	private void CPFvalidateAndEmail(TecnicDTO objDTO) {
+		Optional <People> obj = peopleRepository.findByCpf(objDTO.getCpf());
+		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF already exists!");
+		}
+		
+		obj =  peopleRepository.findByEmail(objDTO.getEmail());
+		
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email already exists!");
+		}
 	}
 
 }
